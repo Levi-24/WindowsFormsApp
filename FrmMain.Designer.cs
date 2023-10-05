6dc36c7db9dc6d6ac308646c3c7db6dd6f082457
Levente Major <leventemajor36@gmail.com>
2023-10-05 10:26:59 +0200

    Add project files.

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTextField = new System.Windows.Forms.Label();
+            this.btnClickMe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTextField
+            // 
+            this.lblTextField.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTextField.Location = new System.Drawing.Point(12, 9);
+            this.lblTextField.Name = "lblTextField";
+            this.lblTextField.Size = new System.Drawing.Size(776, 247);
+            this.lblTextField.TabIndex = 0;
+            this.lblTextField.Text = "Placeholder";
+            this.lblTextField.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnClickMe
+            // 
+            this.btnClickMe.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnClickMe.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnClickMe.Location = new System.Drawing.Point(12, 331);
+            this.btnClickMe.Name = "btnClickMe";
+            this.btnClickMe.Size = new System.Drawing.Size(776, 107);
+            this.btnClickMe.TabIndex = 1;
+            this.btnClickMe.Text = "Click me!";
+            this.btnClickMe.UseVisualStyleBackColor = false;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClickMe);
+            this.Controls.Add(this.lblTextField);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTextField;
+        private System.Windows.Forms.Button btnClickMe;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+            btnClickMe.Click += BtnClickMe_Click;
+        }
+        static Random rnd = new Random();
+
+        private void BtnClickMe_Click(object sender, EventArgs e)
+        {
+            if (lblTextField.Text.Contains("Placeholder"))
+            {
+                lblTextField.Text = "Hello World!";
+            }
+            else
+            {
+                lblTextField.BackColor = Color.FromArgb(
+                    red: rnd.Next(0, 256),
+                    blue: rnd.Next(0, 256),
+                    green: rnd.Next(0, 256)
+                    ) ;
+            }
+        }
+    }
+}
